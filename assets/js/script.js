03f6bb635d6fe493824fef3137361ae107837db8
FranSantander <naturatiendacopiapo@gmail.com>
2022-01-08 01:01:29 -0300

    cuarto commit

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+
+
+    $("a").click(function(){
+        var gato = this.hash
+       
+        $("html, body").animate(
+            {
+                scrollTop: $(gato).offset().top 
+            },
+            800
+            
+
+
+
+        )
+
+
+
+    })
+
+
+
+})
